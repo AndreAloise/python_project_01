80e17f236bf36cd4e8e001513dd87b0cdd69df74
AndreAloise <andre.aloise.77@gmail.com>
2024-09-04 12:24:38 -0300

    Adding features to the restaurant class

--- models/restaurant.py
+++ models/restaurant.py
@@ -16,3 +16,26 @@ class Restaurant:
 		if 0 < rating <= 5:
 			review = Review(client, rating)
 			self._reviews.append(review)
+
+
+	def change_state(self):
+		self._active = not self._active
+
+	@classmethod
+	def get_restaurants(cls):
+		for restaurant in cls.restaurants:
+			print(f'{restaurant._name.ljust(25)} | {restaurant._category.ljust(25)} '
+			      f'| {str(restaurant.average_rating).ljust(25)} | {restaurant.status}')
+
+	@property
+	def average_rating(self):
+		if not self._reviews:
+			return '-'
+		sum_ratings = sum(review.rating for review in self._reviews)
+		rating_qtd = len(self._reviews)
+		average = round(sum_ratings / rating_qtd, 1)
+		return average
+
+	@property
+	def status(self):
+		return 'active' if self._active else 'inactive'
