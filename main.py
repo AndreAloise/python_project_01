6b55c50c80e2276bdc48de3cd61da341235c983e
AndreAloise <andre.aloise.77@gmail.com>
2024-09-05 17:14:16 -0300

    Adding endpoint to get a single restaurant's menu, by its name

--- main.py
+++ main.py
@@ -18,6 +18,19 @@ def get_all_restaurants():
 	return restaurants
 
 
+@app.get('/api/getRestaurant/')
+def get_restaurant(restaurant_name):
+	if is_null_empty_or_whitespace(restaurant_name):
+		return 'Invalid Restaurant Name'
+
+	restaurants = get_restaurant_list()
+	for restaurant in restaurants:
+		if restaurant.restaurant == restaurant_name:
+			return restaurant
+
+	return f'We could\'t find the menu for the restaurant: {restaurant_name}'
+
+
 def get_restaurant_list() -> List[GetRestaurantResponseDto]:
 	file_path = Path('raw_data_jsons/company_data.json')
 	with file_path.open('r', encoding='utf-8') as file:
@@ -34,3 +47,15 @@ def get_restaurant_list() -> List[GetRestaurantResponseDto]:
 		response.append(dto)
 
 	return response
+
+
+def is_null_or_empty(s: str) -> bool:
+	return s is None or s == ''
+
+
+def is_whitespace(s: str) -> bool:
+	return s.strip() == ''
+
+
+def is_null_empty_or_whitespace(s: str) -> bool:
+	return is_null_or_empty(s) or is_whitespace(s)
